92cab76df4d44a79146ff4c60e5e7689d8210b0a
Surajgithub14 <114327205+Surajgithub14@users.noreply.github.com>
2022-09-25 12:40:05 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index(String RollNumber)
+        {
+            return "suraj "  +RollNumber;
+        }
+        public String Index1()
+        {
+            return "suraj1";
+        }
+    }
+}
